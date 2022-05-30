108e3bc2e7fab4c9b293d024d8b32d499e97146d
Subroto Kumar <subrotokumar@outlook.in>
2022-05-31 01:29:20 +0530

    Initial Commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: non_constant_identifier_names
+
+String infura_url =
+    "https://kovan.infura.io/v3/86ac754186244080bfb68ffb8b71cac5";
+
+String owner_private_key =
+    "4243bbe3c7d4f28b7c8af83d0924498088d86eacca85eca512fc2d46664214c4";
+
+String voter_private_key =
+    "a77aff30374dbc0b7f53fa63df75bf20bffa10e5154d98404a9e197c5b6d5250";
+
+String contract_address = "0x0FB4A50033959B176142E1eC6dD58A14f16578cD";
